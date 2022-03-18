15c33959ffdae5b734e21f7dde4ce7a8d45ec440
senior-white-tiger <andrek97412@gmail.com>
2022-03-18 15:16:52 +0700

    updated

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,36 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import { ethereumTestnet, ethereumMainnet } from '../lib/Networks';
+import { openSea } from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: ethereumTestnet,
+  mainnet: ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'YourNftToken',
+  tokenName: 'My NFT Token',
+  tokenSymbol: 'MNT',
+  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  maxSupply: 10000,
+  whitelistSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.09,
+    maxMintAmountPerTx: 5,
+  },
+  // contractAddress: '0xf37cBfad94DFC2aB1cdA6Af80D54b8a04C9ed2B2',
+  contractAddress: '0xA6E75513914A78B6CF2c65b766978c7A9283EA0f',
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: openSea,
+  whitelistAddresses: whitelistAddresses,
+};
+
+export default CollectionConfig;
